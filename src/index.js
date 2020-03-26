b29ab2065104038d0cd5bb036a74843168d3659b
Diego <dwebd0@gmail.com>
2020-03-26 12:26:22 +0100

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import {saludar} from './js/funciones.js';
+import './styles.css';
+import img from './assets/perro.png';
+
+const nombre = 'Martita guapa'; 
+saludar( nombre );
+saludar( nombre );
+saludar( nombre );
